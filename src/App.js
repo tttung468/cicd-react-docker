edefb49428995a9410f5a40e72432dc7ef147a80
ThanhTung468 <tttung468@users.noreply.github.com>
2021-06-04 16:06:53 +0700

    test: fail ci

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Hello ReactJS!
-        </h1>
-        <p>
-          CI/CD with ReactJS and Docker.
-        </p>
+        <h1>Hello world!!!</h1>
+        <p>CI/CD with ReactJS and Docker.</p>
       </header>
     </div>
   );
