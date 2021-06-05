156e618d690efabcbe31eb3f48e74cbb85c84d0e
ThanhTung468 <tttung468@users.noreply.github.com>
2021-06-05 10:27:56 +0700

    test deploy

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+rname $BASH_SOURCE`
+
+docker-compose stop
+docker-compose rm -f
+docker-compose pull
+docker-compose up -d
+
+cd - > /dev/null
--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello ReactJs!</h1>
-        <p>CI/CD with ReactJS and Docker.</p>
+        <p>Hello world!! CI/CD with ReactJS and Docker.</p>
       </header>
     </div>
   );
