8bd50807b5b652c39dee6c4f055b23b3a4f0ce5a
ThanhTung468 <tttung468@users.noreply.github.com>
2021-06-06 20:50:43 +0700

    test ci

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello ReactJs!</h1>
+        <h1>Hello ReactJssssss!</h1>
         <p>Hello world!! CI/CD with ReactJS and Docker.</p>
       </header>
     </div>
